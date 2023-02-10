538a9b3f9573bb3bc2897e38b5ded093dc5a314e
julianngrulla <119913955+julianngrulla@users.noreply.github.com>
2023-02-10 14:28:31 +0800

    Add files via upload

---
+++ Exercise5/Daddy.java
@@ -0,0 +1,10 @@
+public class Daddy {
+
+    protected String surname = "Grulla";
+
+    protected void showSurname(){
+        System.out.println("My family name is: " + surname);
+    }
+
+    
+}
